d85aae7e93c87d0ae6237e7141d9793ab488c15a
rafaelcr11 <rribeiro.inmetrics@digio.com.br>
2021-11-09 16:57:38 -0300

    Projeto completo

---
+++ SalesWebMvc/Migrations/20211103193224_OtherMigrationsCorrection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherMigrationsCorrection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "BaseSalary",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BaseSalary",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(double));
+        }
+    }
+}
